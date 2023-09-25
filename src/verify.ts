944f56fc15c870a81ed885337e0c36c381ee3c86
Denis Carriere <carriere.denis@gmail.com>
2023-09-25 16:52:03 -0400

    add verify tests

--- src/verify.ts
+++ src/verify.ts
@@ -1,7 +1,7 @@
 import nacl from "tweetnacl";
 
 // validate signature using public key
-export async function verify(body: string, timestamp: string, signature: string, PUBLIC_KEY: string){
+export async function verify(body: string, timestamp: number, signature: string, PUBLIC_KEY: string){
     return nacl.sign.detached.verify(
         Buffer.from(timestamp + body),
         Buffer.from(signature, 'hex'),
