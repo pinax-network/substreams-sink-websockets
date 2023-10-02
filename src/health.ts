6df8d870432dd9612c74ccb549a899655d66aead
Samuel Papineau <samuelpapineau08@gmail.com>
2023-10-02 12:26:35 -0400

    Fixed health endpoint

--- src/health.ts
+++ src/health.ts
@@ -2,7 +2,8 @@ import { Database } from "bun:sqlite";
 import * as sqlite from "./sqlite.js";
 
 export async function checkHealth(db: Database) {
-    const total_trace_id = sqlite.findAll(db, "traceId").length;
+    const total_trace_id = sqlite.selectAll(db, "traceId").length;
+    console.log('total_trace_id', total_trace_id)
     if (total_trace_id) return {data: "OK", status: { status: 200 }};
     return {data: "Error: No connected webhooks", status: { status: 400 }};
 
