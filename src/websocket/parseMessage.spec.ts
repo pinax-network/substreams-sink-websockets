3325d488e8794ddab48b3e244598a4d4f766ef86
Denis Carriere <carriere.denis@gmail.com>
2023-10-08 13:53:30 -0400

    Add unit tests for parseMessage fixes: https://github.com/pinax-network/substreams-sink-websockets/issues/11

--- src/websocket/parseMessage.spec.ts
+++ src/websocket/parseMessage.spec.ts
@@ -1,7 +1,64 @@
 import { expect, test } from "bun:test";
 import { parseMessage } from "./parseMessage.js";
 
-test("parseMessage", () => {
-    // TO-DO
-    expect(true).toBeTruthy();
+test("parseMessage - subscribe to ModuleHash by Chain", () => {
+    expect(parseMessage(`{
+        "method": "subscribe",
+        "params": {
+            "chain": "bsc",
+            "moduleHash": "0a363b2a63aadb76a525208f1973531d3616fbae"
+        }
+    }`)).toBeTruthy();
+});
+
+test("parseMessage - subscribe to ModuleHash", () => {
+    expect(parseMessage(`{
+        "method": "subscribe",
+        "params": {
+            "moduleHash": "0a363b2a63aadb76a525208f1973531d3616fbae"
+        }
+    }`)).toBeTruthy();
+});
+
+test("parseMessage - Missing required 'params' in JSON request.", () => {
+    expect(() => parseMessage(`{
+        "method": "subscribe"
+    }`)).toThrow("Missing required 'params' in JSON request.");
+});
+
+test("parseMessage - Missing required 'moduleHash' in JSON request.", () => {
+    expect(() => parseMessage(`{
+        "method": "subscribe",
+        "params": {
+            "chain": "bsc"
+        }
+    }`)).toThrow("Missing required 'moduleHash' in JSON request.");
+});
+
+test("parseMessage - ping", () => {
+    expect(parseMessage(`{
+        "method": "ping"
+    }`)).toBeTruthy();
+
+    expect(parseMessage(`{
+        "id": 123,
+        "method": "ping"
+    }`).id).toBe(123);
+});
+
+test("parseMessage - time", () => {
+    expect(parseMessage(`{
+        "method": "time"
+    }`)).toBeTruthy();
+
+    expect(parseMessage(`{
+        "id": 123,
+        "method": "time"
+    }`).id).toBe(123);
+});
+
+test("parseMessage - Missing required 'method' in JSON request.", () => {
+    expect(() => parseMessage(`{
+        "foo": "bar"
+    }`)).toThrow("Missing required 'method' in JSON request.");
 });
--- src/websocket/parseMessage.ts
+++ src/websocket/parseMessage.ts
@@ -6,7 +6,14 @@ export function parseMessage(message: string|Buffer): { method: string, id: stri
     } catch (error) {
         throw new Error("Invalid JSON message");
     }
-    const { method } = json;
+    const { method, params } = json;
     if ( method === null || method === undefined) throw new Error('Missing required \'method\' in JSON request.');
+
+    if ( method === 'subscribe' ) {
+        if ( params === null || params === undefined) throw new Error('Missing required \'params\' in JSON request.');
+        const { moduleHash } = params;
+        if ( moduleHash === null || moduleHash === undefined) throw new Error('Missing required \'moduleHash\' in JSON request.');
+    }
+
     return json;
 }
