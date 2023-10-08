f6be40d7ce8fbd16ead6336b892fff4d9d6de150
Denis Carriere <carriere.denis@gmail.com>
2023-10-07 22:39:55 -0400

    change response format

--- src/websocket/ping.ts
+++ src/websocket/ping.ts
@@ -6,6 +6,6 @@ import * as prometheus from "../prometheus.js";
 // https://developers.binance.com/docs/binance-trading-api/websocket_api#test-connectivity
 export default function (ws: ServerWebSocket<ServerWebSocketData>, id?: string) {
     prometheus.pings.inc(1);
-    ws.send(JSON.stringify({id, status: 200, result: {}}));
+    ws.send(JSON.stringify({id, status: 200}));
     logger.info('ping', {id, key: ws.data.key, remoteAddress: ws.remoteAddress});
 }
--- src/websocket/time.ts
+++ src/websocket/time.ts
@@ -6,6 +6,6 @@ import * as prometheus from "../prometheus.js";
 // https://developers.binance.com/docs/binance-trading-api/websocket_api#check-server-time
 export default function (ws: ServerWebSocket<ServerWebSocketData>, id?: string) {
     const serverTime = Number(Date.now());
-    ws.send(JSON.stringify({id, status: 200, result: {serverTime}}));
+    ws.send(JSON.stringify({id, status: 200, data: serverTime}));
     logger.info('time', {id, key: ws.data.key, remoteAddress: ws.remoteAddress});
 }
