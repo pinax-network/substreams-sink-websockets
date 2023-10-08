99850765dfebfd9c07c9321227d2729c3b4f0d3a
Denis Carriere <carriere.denis@gmail.com>
2023-10-08 15:02:59 -0400

    add msg to ping

--- src/websocket/ping.ts
+++ src/websocket/ping.ts
@@ -4,6 +4,6 @@ import { ServerWebSocketData } from "../../index.js";
 
 // https://developers.binance.com/docs/binance-trading-api/websocket_api#test-connectivity
 export default function (ws: ServerWebSocket<ServerWebSocketData>, id?: string) {
-    ws.send(JSON.stringify({id, status: 200, data: {}}));
+    ws.send(JSON.stringify({id, status: 200, data: {msg: "pong"}}));
     logger.info('ping', {id, key: ws.data.key, remoteAddress: ws.remoteAddress});
 }
